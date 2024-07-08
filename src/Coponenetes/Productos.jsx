4ce6ea62ca02cf940c62381b29e84a885b4e335a
Angel0956 <146665441+Angel0956@users.noreply.github.com>
2024-07-08 00:50:17 -0500

    modifique

--- src/Coponenetes/Productos.jsx
+++ src/Coponenetes/Productos.jsx
@@ -28,7 +28,7 @@ function Producto(props) {
           >
             <Heading>{Name}</Heading>
             <Text>Precio: ${Precio}$</Text>
-            <Text>Hola que hay</Text>
+           
             <Button >Añadir</Button>
           </CardBody>
         </HStack>
